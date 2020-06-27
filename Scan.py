b115f2bcb6d3e5a4b4505351c1ca5bef13e914c8
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-06-27 19:08:25 -0400

    alpaca api isn't working as it was yesterday, still got some work done.

--- Scan.py
+++ Scan.py
@@ -15,9 +15,8 @@ class Scan:
 
     def __init__(self, symbol):
         self.symbol = symbol
-        self.alpacaAPI = alpaca_trade_api.REST(ALPACA_PUBLIC_KEY, ALPACA_PRIVATE_KEY, api_version="v2")
         self.currentTime = datetime.datetime.now()
-        self.testingTime = datetime.datetime(2020, 6, 26, 13, 49)
+        self.testingTime = datetime.datetime(2020, 6, 26, 15, 40)
         self.isMarketOpen = True
         self.nextOpen = None
         self.nextClose = None
@@ -28,6 +27,8 @@ class Scan:
         self.candleColor = None
         self.latestCandle = None
         self.lastXClosingPrices = None
+        self.currentTrade = None #trade object trade object needs to change this variable when in closes itself.
+        self.bias = None
 
     def lookForTrades(self):
         if self.isMarketOpen:
@@ -44,30 +45,39 @@ class Scan:
 
     def isSignal(self):
         toPositive = lambda x: x * -1
-        barSize = self.latestCandle.o - self.latestCandle.c
+        subNoNegatives = lambda x, y: max(x, y) - min(x, y)
+        # barSize = self.latestCandle.o - self.latestCandle.c
+        # barSize = self.latestCandle.o - self.latestCandle.c if self.latestCandle.o > self.latestCandle.c else self.latestCandle.c - self.latestCandle.o
+        barSize = subNoNegatives(self.latestCandle.o, self.latestCandle.c)
         minimumReq = barSize * 0.501
         self.SMA = sum(self.lastXClosingPrices) / len(self.lastXClosingPrices)
-        smaHeight = self.SMA - self.latestCandle.c
-        outsideSMA = False
+        # smaHeight = self.SMA - self.latestCandle.c
+        smaHeight = subNoNegatives(self.SMA, self.latestCandle.c)
         insideSMA = True
         above = below = None
 
-        if barSize <= 0:
-            barSize = toPositive(barSize)
-            minimumReq = toPositive(barSize)
-        if smaHeight < 0:
-            smaHeight = toPositive(smaHeight)
+        # if barSize <= 0:
+        #     barSize = toPositive(barSize)
+        #     minimumReq = toPositive(barSize)
+        # if smaHeight < 0:
+        #     smaHeight = toPositive(smaHeight)
+
         if smaHeight > barSize or smaHeight < 0:
             print("out of bounds")
-            outsideSMA = True
-            insideSMA = not outsideSMA
+            insideSMA = False
             above = smaHeight < 0
             below = not above
 
         viableCrossover = smaHeight > minimumReq and insideSMA
-
-
-
+        if above:
+            return "bull"
+        elif below:
+            return "bear"
+        elif viableCrossover:
+            if (self.SMA - min(self.latestCandle.o , self.latestCandle.c)) < (max(self.latestCandle.c , self.latestCandle.o) - self.SMA):
+                return "bull"
+            else:
+                return "bear"
 
     def onTick(self):
         # once awake, check prices, get new SMA, check for a signal
@@ -76,12 +86,17 @@ class Scan:
         self.latestCandle = Scan.alpacaAPI.get_barset(symbols=self.symbol, limit=1, end="{}-04:00".format(self.testingTime.strftime(Scan.alpacaDateTimeFormat)), timeframe="minute")[self.symbol][0]
         self.lastXClosingPrices.pop(0)
         self.lastXClosingPrices.append(self.latestCandle.c)
-        self.candleColor = "green" if self.latestCandle.o < self.latestCandle.c else "red"
-        if self.isSignal():
-            # do something
-            pass
-        else:
-            # do somethings else, proably sleep
+        self.bias = self.isSignal()
+        if self.bias == "bull":
+            if not self.openTrade:
+                #open trade
+                pass
+            elif self.currentTrade != self.bias: #.side #.isOpen
+                #close position and open opposing position
+                pass
+            # sleep to next minute on conformation of trade
+        elif "bear":  # do somethings else, proably slee
+            # can probably be done in one block
             pass
 
     def checkMarketConditions(self):
@@ -92,6 +107,7 @@ class Scan:
 
     def getClosingPrices(self, amount: int) -> list:
         timedelta = datetime.timedelta(minutes=1)
+        print("here")
         candles = Scan.alpacaAPI.get_barset(symbols=self.symbol, limit=amount, end="{}-04:00".format((self.testingTime - timedelta).strftime(Scan.alpacaDateTimeFormat)),
                                             timeframe="minute")[self.symbol]
         return [i.c for i in candles]
@@ -99,7 +115,6 @@ class Scan:
     @staticmethod
     def KillSwitch(self):
         pass
-
 
 if __name__ == '__main__':
     scan = Scan("SPY")
