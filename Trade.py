636f5ab452a0b544b03a481bef5b65b71ebcfd11
kamalh2001 <63170492+kamalh2001@users.noreply.github.com>
2020-07-05 23:58:20 -0400

    simplfied and fixed trade object, also update gitignore

--- Trade.py
+++ Trade.py
@@ -5,42 +5,40 @@ import threading
 
 class Trade(threading.Thread):
     def __init__(self, scanObject, api: alpaca_trade_api.REST, bias):
+        # self.close_all_positions is a massive bottleneck that will make it impossible to scan
         super().__init__()
         self.master = scanObject
         self.side = "buy" if self.master.bias == "bull" else "sell"
         self.bias = bias
         self.api = api
         self.order = None
-        self.monitor = None
         self.stopSignal = threading.Event()
         self.sleepLock = threading.Condition(threading.Lock())
         self.lossTooBig = -.50
         self.trailingStop = None
         self.trailingStopLast = 0
+        self.currentPrice = 0
 
     def openAndMonitor(self):
         # market orders for now.
         self.order = self.api.submit_order(symbol=self.master.symbol, qty=1, side=self.side, type="market", time_in_force="day")
+        self.order = self.api.get_order(self.order.id)
         self.start()
+
     def close(self):
         self.api.close_all_positions()
+        self.master.currentTrade = None
 
     def run(self):
+        self.trailingStop = self.order.filled_avg_price - self.lossTooBig
         while not self.stopSignal.isSet():
-            self.monitor = self.api.get_position(self.master.symbol)
-            print(self.monitor)
-            if float(self.monitor.unrealized_pl) < self.lossTooBig:
+            self.currentPrice = self.api.get_last_quote(self.master.symbol)
+            if float(self.currentPrice) < self.trailingStop:
                 print("closing due to intial stop price hit")
-                self.api.close_all_positions()
-                self.master.currentTrade = None
-                break
-            elif self.monitor.current_price < self.trailingStop:
-                print("trailing stop hit")
-                self.api.close_all_positions()
-                self.master.currentTrade = None
+                self.close()
                 break
             else:
-                self.trailingStop = self.monitor.current_price - .50
+                self.trailingStop = self.currentPrice - .50
                 with self.sleepLock:
                     self.sleepLock.wait(.75)
 
